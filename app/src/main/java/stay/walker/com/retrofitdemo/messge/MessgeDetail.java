4bdfc8a723ec82a7fe4752ab6b313013f63d70e9
walker <597222089@qq.com>
2018-09-10 20:00:16 +0800

    MessageDetail 1.1

--- app/src/main/java/stay/walker/com/retrofitdemo/messge/MessgeDetail.java
+++ app/src/main/java/stay/walker/com/retrofitdemo/messge/MessgeDetail.java
@@ -1,8 +1,19 @@
 package stay.walker.com.retrofitdemo.messge;
 
+import stay.walker.com.retrofitdemo.dialog.LoadingDialog;
+import stay.walker.com.retrofitdemo.log.LogDetail;
+
 /**
  * @author walker
  * 消息处理
  */
 public class MessgeDetail {
+
+    public static void receiver() {
+        LogDetail.V();
+    }
+
+    public static void recvNetworkMsg () {
+        LogDetail.Debug();
+    }
 }
