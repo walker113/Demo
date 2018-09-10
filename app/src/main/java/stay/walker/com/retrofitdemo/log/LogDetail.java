51a5b93f2ff697f5bf757cd190223cc981c90817
walker <597222089@qq.com>
2018-09-10 19:51:15 +0800

    LogDetail 1.1

---
+++ app/src/main/java/stay/walker/com/retrofitdemo/log/LogDetail.java
@@ -0,0 +1,13 @@
+package stay.walker.com.retrofitdemo.log;
+
+
+
+/**
+ * 日志处理
+ * @author walker
+ */
+public class LogDetail {
+    public static void D () {
+        System.out.println();
+    }
+}
