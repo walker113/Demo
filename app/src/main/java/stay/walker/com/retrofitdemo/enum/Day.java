03b3b22707d7c37974ca21d2adb70f66ddd5956e
zhangzanlei <597222089@qq.com>
2018-09-13 16:14:55 +0800

    commit common methods of Enum

--- app/src/main/java/stay/walker/com/retrofitdemo/enum/Day.java
+++ app/src/main/java/stay/walker/com/retrofitdemo/enum/Day.java
@@ -4,40 +4,40 @@
 // 编译器还为我们生成了两个静态方法，分别是values()和valueOf()
 // 
 // 使用关键字enum定义的枚举类型，在编译后，也将转化成一个实实在在的类；
-final class Day extends Enum {
-
-    public static Day[] values() {
-        return (Day[])$VALUES.clone();
-    }
-
-
-    // 私有构造函数
-    private Day(String s, int i) {
-        super(s, i);
-    }
-
-    // 所定义的7种类型
-    public static final Day MONDAY;
-    public static final Day TUESDAY;
-    public static final Day WEDNESDAY;
-    public static final Day THURSDAY;
-    public static final Day FRIDAY;
-    public static final Day SATURDAY;
-    public static final Day SUNDAY;
-    private static final Day $VALUES[];
-
-    static {
-        MONDAY = new Day("MONDAY", 0);
-        TUESDAY = new Day("TUESDAY", 1);
-        WEDNESDAY = new Day("WEDNESDAY", 2);
-        THURSDAY = new Day("THURSDAY", 3);
-        FRIDAY = new Day("FRIDAY", 4);
-        SATURDAY = new Day("SATURDAY", 5);
-        SUNDAY = new Day("SUNDAY", 6);
-
-        $VALUES = (new Day[]{
-           MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
-        });
-    }
-
-}
+//final class Day extends Enum {
+//
+//    public static Day[] values() {
+//        return (Day[])$VALUES.clone();
+//    }
+//
+//
+//    // 私有构造函数
+//    private Day(String s, int i) {
+//        super(s, i);
+//    }
+//
+//    // 所定义的7种类型
+//    public static final Day MONDAY;
+//    public static final Day TUESDAY;
+//    public static final Day WEDNESDAY;
+//    public static final Day THURSDAY;
+//    public static final Day FRIDAY;
+//    public static final Day SATURDAY;
+//    public static final Day SUNDAY;
+//    private static final Day $VALUES[];
+//
+//    static {
+//        MONDAY = new Day("MONDAY", 0);
+//        TUESDAY = new Day("TUESDAY", 1);
+//        WEDNESDAY = new Day("WEDNESDAY", 2);
+//        THURSDAY = new Day("THURSDAY", 3);
+//        FRIDAY = new Day("FRIDAY", 4);
+//        SATURDAY = new Day("SATURDAY", 5);
+//        SUNDAY = new Day("SUNDAY", 6);
+//
+//        $VALUES = (new Day[]{
+//           MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+//        });
+//    }
+//
+//}
--- app/src/main/java/stay/walker/com/retrofitdemo/enum/EnumDemo.java
+++ app/src/main/java/stay/walker/com/retrofitdemo/enum/EnumDemo.java
@@ -1,12 +1,50 @@
 import java.lang.*;
+import java.util.Arrays;
 
+/**
+ * @author 009632
+ */
 public class EnumDemo {
     public static void main(String[] args) {
-        Day day = Day.MONDAY;
+        //创建枚举数组
+        Day[] days = new Day[]{Day.MONDAY, Day.TUESDAY, Day.WEDNESDAY,
+                Day.THURSDAY, Day.FRIDAY, Day.SATURDAY, Day.SUNDAY};
 
-        System.out.println(day.name());
-        System.out.println(day.ordinal());
-        System.out.println(day.toString());
+        for (int i = 0; i <days.length ; i++) {
+            System.out.println("day["+i+"].ordinal():"+days[i].ordinal());
+        }
+
+        System.out.println("-------------------------------------");
+        //通过compareTo方法比较,实际上其内部是通过ordinal()值比较的
+        System.out.println("days[0].compareTo(days[1]):"+days[0].compareTo(days[1]));
+        System.out.println("days[0].compareTo(days[1]):"+days[0].compareTo(days[2]));
+
+
+        //获取该枚举对象的Class对象引用,当然也可以通过getClass方法
+        Class<?> clazz = days[0].getDeclaringClass();
+        System.out.println("clazz:"+clazz);
+
+        System.out.println("-------------------------------------");
+
+        Day d=Enum.valueOf(Day.class,days[0].name());
+        Day d2=Day.valueOf(Day.class,days[0].name());
+        System.out.println("d:"+d);
+        System.out.println("d2:"+d2);
+
+
+        System.out.println("-------------------------------------");
+        //正常使用
+        Day[] ds=Day.values();
+        //向上转型Enum
+        Enum e = Day.MONDAY;
+        //无法调用,没有此方法
+        //e.values();
+        //获取class对象引用
+        Class<?> clasz = e.getDeclaringClass();
+        if(clasz.isEnum()) {
+            Day[] dsz = (Day[]) clasz.getEnumConstants();
+            System.out.println("dsz:"+ Arrays.toString(dsz));
+        }
     }
 }
 
